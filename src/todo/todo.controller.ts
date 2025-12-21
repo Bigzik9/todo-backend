1496d2cc6b465e8b17611a412570c5e61845ee65
Bigzik9 <isaac.dominic07@gmail.com>
2025-12-21 17:53:11 +0100

    register todo module in app module

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
 
-@Controller('todo')
-export class TodoController {}
+
+@Controller('todos')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Post()
+create(@Body() body: any) {
+  return this.todoService.create(body);
+}
+
+@Get()
+findAll() {
+  return this.todoService.findAll();
+}
+
+@Get(':id')
+findOne(@Param('id') id: string) {
+  return this.todoService.findOne(id);
+}
+
+@Put(':id')
+update(@Param('id') id: string, @Body() body: any) {
+  return this.todoService.update(id, body);
+}
+
+@Delete(':id')
+remove(@Param('id') id: string) {
+  return this.todoService.remove(id);
+}
+
+}
+
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  create(data: any) {
+    return { message: 'Todo created', data };
+  }
+
+  findAll() {
+    return [];
+  }
+
+  findOne(id: string) {
+    return { id };
+  }
+
+  update(id: string, data: any) {
+    return { id, ...data };
+  }
+
+  remove(id: string) {
+    return { message: 'Todo deleted', id };
+  }
+}
+
