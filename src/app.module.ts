3ace4384a0ba2b50fcd7c3e962180b223ec1fd7b
Bigzik9 <isaac.dominic07@gmail.com>
2025-12-22 11:22:23 +0100

    downgrade to stable Prisma v5 and initialize todo schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/todo_db'), 
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
