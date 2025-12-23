1c09c72cc7461746362eee22a4cf52f00cc679cf
Bigzik9 <isaac.dominic07@gmail.com>
2025-12-23 18:22:55 +0100

    updated main.ts to allow cors work with my frontend

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Vite default
+    origin: [
+      'http://localhost:3000',
+      'https://todo-frontend-ten-beta.vercel.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
